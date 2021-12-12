77aaa10efb06c6fa03447abf7258390b702969b0
nguyen-william93 <=>
2021-12-12 15:17:56 -0600

    saving file

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    //first test
+    it('render', () => {
+        render(<About />)
+    });
+
+    //second test
+})
