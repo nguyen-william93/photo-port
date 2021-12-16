a1bbac365fa7dd90b37164def0828a0866de239c
nguyen-william93 <=>
2021-12-15 18:56:38 -0600

    update json package and test file for contact

--- src/components/Contact/__tests__/index.test.js
+++ src/components/Contact/__tests__/index.test.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import {render, cleanup} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import ContactForm from '..';
+import Contact from '../../Contact';
 
 afterEach(cleanup);
 
 describe('Contact component', () => {
     //baseline
     it('render', () => {
-        render(<ContactForm />)
+        render(<Contact />)
     });
 
     it('match Snapshot', () => {
-        const {asFragment} = render(<ContactForm />)
+        const {asFragment} = render(<Contact />)
         expect(asFragment()).toMatchSnapshot();
     })
 })
 
 it('renders', () => {
-    const {getByTestId} = render(<ContactForm />)
+    const {getByTestId} = render(<Contact />)
     expect(getByTestId('h1tag')).toHaveTextContent('Contact me');
 })
 
 it('renders', () => {
-    const {getByTestId} = render(<ContactForm />)
+    const {getByTestId} = render(<Contact />)
     expect (getByTestId('button')).toHaveTextContent('Submit')
 })
